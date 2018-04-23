e0489192e4b1f1acc6129c564e67f442d70620ab
Zulolo <dodoleon@sina.com>
2018-04-23 23:03:03 +0800

    display not working

--- main/display.c
+++ main/display.c
@@ -10,131 +10,136 @@
 #include "soc/gpio_struct.h"
 #include "driver/gpio.h"
 
-#define PIN_NUM_MISO 		12
-#define PIN_NUM_MOSI 		13
-#define PIN_NUM_CLK  		14
-#define PIN_NUM_CS   		15
-#define PIN_NUM_RST   		32
-#define PIN_NUM_BCKL   		33
+#define DISPLAY_PIN_NUM_MISO 		12
+#define DISPLAY_PIN_NUM_MOSI 		13
+#define DISPLAY_PIN_NUM_CLK  		14
+#define DISPLAY_PIN_NUM_CS   		15
+#define DISPLAY_PIN_NUM_RST   		32
+#define DISPLAY_PIN_NUM_BCKL   		33
 
-static const char *TAG_DISPLAY = "[WEBSERVER]";
-
+static const char *TAG_DISPLAY = "[DISPLAY]";
 /*
  The ILI9341 needs a bunch of command/argument values to be initialized. They are stored in this struct.
-*/
+ */
 typedef struct {
-    uint8_t cmd;
-    uint8_t data[16];
-    uint8_t databytes; //No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
+	uint8_t cmd;
+	uint8_t data[16];
+	uint8_t databytes; //No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
 } ili_init_cmd_t;
 
-//Place data into DRAM. Constant data gets placed into DROM by default, which is not accessible by DMA.
-DRAM_ATTR static const ili_init_cmd_t ili_init_cmds[]={
-    {0xCF, {0x00, 0x83, 0X30}, 3},
-    {0xED, {0x64, 0x03, 0X12, 0X81}, 4},
-    {0xE8, {0x85, 0x01, 0x79}, 3},
-    {0xCB, {0x39, 0x2C, 0x00, 0x34, 0x02}, 5},
-    {0xF7, {0x20}, 1},
-    {0xEA, {0x00, 0x00}, 2},
-    {0xC0, {0x26}, 1},
-    {0xC1, {0x11}, 1},
-    {0xC5, {0x35, 0x3E}, 2},
-    {0xC7, {0xBE}, 1},
-    {0x36, {0x28}, 1},
-    {0x3A, {0x55}, 1},
-    {0xB1, {0x00, 0x1B}, 2},
-    {0xF2, {0x08}, 1},
-    {0x26, {0x01}, 1},
-    {0xE0, {0x1F, 0x1A, 0x18, 0x0A, 0x0F, 0x06, 0x45, 0X87, 0x32, 0x0A, 0x07, 0x02, 0x07, 0x05, 0x00}, 15},
-    {0XE1, {0x00, 0x25, 0x27, 0x05, 0x10, 0x09, 0x3A, 0x78, 0x4D, 0x05, 0x18, 0x0D, 0x38, 0x3A, 0x1F}, 15},
-    {0x2A, {0x00, 0x00, 0x00, 0xEF}, 4},
-    {0x2B, {0x00, 0x00, 0x01, 0x3f}, 4},
-    {0x2C, {0}, 0},
-    {0xB7, {0x07}, 1},
-    {0xB6, {0x0A, 0x82, 0x27, 0x00}, 4},
-    {0x11, {0}, 0x80},
-    {0x29, {0}, 0x80},
-    {0, {0}, 0xff},
-};
-
-static spi_bus_config_t buscfg={
-    .miso_io_num=PIN_NUM_MISO,
-    .mosi_io_num=PIN_NUM_MOSI,
-    .sclk_io_num=PIN_NUM_CLK,
-    .quadwp_io_num=-1,
-    .quadhd_io_num=-1
-};
+static spi_bus_config_t buscfg = { .miso_io_num = DISPLAY_PIN_NUM_MISO, .mosi_io_num = DISPLAY_PIN_NUM_MOSI,
+		.sclk_io_num = DISPLAY_PIN_NUM_CLK, .quadwp_io_num = -1, .quadhd_io_num = -1 };
 
 static void display_pre_transfer_callback(spi_transaction_t *t) {
-    // do something?
+	// do something?
 }
 
-static spi_device_interface_config_t devcfg={
-    .clock_speed_hz=10000000,
-    .mode=0,                                //SPI mode 0
-    .spics_io_num=PIN_NUM_CS,               //CS pin
-    .queue_size=7,                          //We want to be able to queue 7 transactions at a time
-    .pre_cb=display_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
-};
+static spi_device_interface_config_t devcfg = { .clock_speed_hz = 1000000, .mode = 0,  //SPI mode 0
+		.spics_io_num = -1,               //CS pin
+		.queue_size = 7,                      //We want to be able to queue 7 transactions at a time
+		.pre_cb = display_pre_transfer_callback, //Specify pre-transfer callback to handle D/C line
+		};
 
-//Send a command to the ILI9341. Uses spi_device_transmit, which waits until the transfer is complete.
-void ili_cmd(spi_device_handle_t spi, const uint8_t cmd)
-{
-    esp_err_t ret;
-    spi_transaction_t t;
-    memset(&t, 0, sizeof(t));       //Zero out the transaction
-    t.length=8;                     //Command is 8 bits
-    t.tx_buffer=&cmd;               //The data is the cmd itself
-    t.user=(void*)0;                //D/C needs to be set to 0
-    ret=spi_device_transmit(spi, &t);  //Transmit!
-    assert(ret==ESP_OK);            //Should have had no issues.
+static void send_cmd(spi_device_handle_t spi, const uint8_t cmd) {
+	spi_transaction_t t;
+	static uint8_t sendbyte[3];
+	ESP_LOGI(TAG_DISPLAY, "Send cmd 0x%02X.", cmd);
+	memset(&t, 0, sizeof(t));
+	sendbyte[0] = 0xf8;
+	sendbyte[1] = cmd & 0xF0;
+	sendbyte[2] = (cmd << 4) & 0xF0;
+
+	t.length = sizeof(sendbyte);
+	t.tx_buffer = sendbyte;
+	gpio_set_level(DISPLAY_PIN_NUM_CS, 1);
+	ESP_ERROR_CHECK(spi_device_transmit(spi, &t));
+	vTaskDelay(10 / portTICK_RATE_MS);
+	gpio_set_level(DISPLAY_PIN_NUM_CS, 0);
 }
 
-//Send data to the ILI9341. Uses spi_device_transmit, which waits until the transfer is complete.
-void ili_data(spi_device_handle_t spi, const uint8_t *data, int len)
-{
-    esp_err_t ret;
-    spi_transaction_t t;
-    if (len==0) return;             //no need to send anything
-    memset(&t, 0, sizeof(t));       //Zero out the transaction
-    t.length=len*8;                 //Len is in bytes, transaction length is in bits.
-    t.tx_buffer=data;               //Data
-    t.user=(void*)1;                //D/C needs to be set to 1
-    ret=spi_device_transmit(spi, &t);  //Transmit!
-    assert(ret==ESP_OK);            //Should have had no issues.
+static void send_data(spi_device_handle_t spi, const uint8_t data) {
+	spi_transaction_t t;
+	static uint8_t sendbyte[3];
+	ESP_LOGI(TAG_DISPLAY, "Send data 0x%02X.", data);
+	memset(&t, 0, sizeof(t));
+	sendbyte[0] = 0xFA;
+	sendbyte[1] = data & 0xF0;
+	sendbyte[2] = (data << 4) & 0xF0;
+
+	t.length = sizeof(sendbyte);
+	t.tx_buffer = sendbyte;
+	gpio_set_level(DISPLAY_PIN_NUM_CS, 1);
+	ESP_ERROR_CHECK(spi_device_transmit(spi, &t));
+	vTaskDelay(10 / portTICK_RATE_MS);
+	gpio_set_level(DISPLAY_PIN_NUM_CS, 0);
 }
 
-static diaplay_spi_init(void) {
-    esp_err_t ret;
-    spi_device_handle_t spi;
-
-    ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
-
-    ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
-
-    int cmd=0;
-    //Initialize non-SPI GPIOs
-    gpio_set_direction(PIN_NUM_RST, GPIO_MODE_OUTPUT);
-    gpio_set_direction(PIN_NUM_BCKL, GPIO_MODE_OUTPUT);
-    //Reset the display
-    gpio_set_level(PIN_NUM_RST, 0);
-    vTaskDelay(100 / portTICK_RATE_MS);
-    gpio_set_level(PIN_NUM_RST, 1);
-    vTaskDelay(100 / portTICK_RATE_MS);
-    //Send all the commands
-    while (ili_init_cmds[cmd].databytes!=0xff) {
-        ili_cmd(spi, ili_init_cmds[cmd].cmd);
-        ili_data(spi, ili_init_cmds[cmd].data, ili_init_cmds[cmd].databytes&0x1F);
-        if (ili_init_cmds[cmd].databytes&0x80) {
-            vTaskDelay(100 / portTICK_RATE_MS);
-        }
-        cmd++;
+void display_putstr(spi_device_handle_t spi, const char *str) {
+    uint8_t *s = (uint8_t*)str;
+    while(*s > 0) {
+    	send_data(spi, *s);
+        s++;
+        vTaskDelay(50 / portTICK_RATE_MS);
     }
-    ///Enable backlight
-    gpio_set_level(PIN_NUM_BCKL, 1);
+}
+
+void display_clear(spi_device_handle_t spi) {
+	send_cmd(spi, 0x01);
+	vTaskDelay(50 / portTICK_RATE_MS);
+}
+
+void display_setpos(spi_device_handle_t spi, int y, int x) {
+    switch(y) {
+        case 0:
+        	send_cmd(spi, 0x80 + x);
+            return;
+        case 1:
+        	send_cmd(spi, 0x90 + x);
+            return;
+        case 2:
+        	send_cmd(spi, 0x88 + x);
+            return;
+        case 3:
+        	send_cmd(spi, 0x98 + x);
+            return;
+    }
+}
+
+static void diaplay_spi_init(spi_device_handle_t spi) {
+	ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
+
+	ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
+
+	//Initialize non-SPI GPIOs
+	gpio_set_direction(DISPLAY_PIN_NUM_RST, GPIO_MODE_OUTPUT);
+	gpio_set_direction(DISPLAY_PIN_NUM_BCKL, GPIO_MODE_OUTPUT);
+	gpio_set_direction(DISPLAY_PIN_NUM_CS, GPIO_MODE_OUTPUT);
+	gpio_set_level(DISPLAY_PIN_NUM_CS, 0);
+	//Reset the display
+	gpio_set_level(DISPLAY_PIN_NUM_RST, 0);
+	vTaskDelay(100 / portTICK_RATE_MS);
+	gpio_set_level(DISPLAY_PIN_NUM_RST, 1);
+	vTaskDelay(1000 / portTICK_RATE_MS);
+
+	//Send all the commands
+	send_cmd(spi, 0x30);
+	vTaskDelay(100 / portTICK_RATE_MS);
+	send_cmd(spi, 0x0C);
+	vTaskDelay(100 / portTICK_RATE_MS);
+	display_clear(spi);
+	display_setpos(spi, 0, 0);
+	display_putstr(spi, "Hello world!");
+	///Enable backlight
+	gpio_set_level(DISPLAY_PIN_NUM_BCKL, 1);
 }
 
 void display_task(void *pvParameters) {
+	static spi_device_handle_t spi;
+	diaplay_spi_init(spi);
 
+	while (1) {
+
+		vTaskDelay(2000 / portTICK_RATE_MS);
+	}
 
 }
