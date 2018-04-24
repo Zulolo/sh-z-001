a161e2b51255e6cb1fecc52f07c4151ec72c46f5
Zulolo <dodoleon@sina.com>
2018-04-24 21:44:04 +0800

    display working

--- main/display.c
+++ main/display.c
@@ -17,6 +17,7 @@
 #define DISPLAY_PIN_NUM_RST   		32
 #define DISPLAY_PIN_NUM_BCKL   		33
 
+static spi_device_handle_t spi;
 static const char *TAG_DISPLAY = "[DISPLAY]";
 /*
  The ILI9341 needs a bunch of command/argument values to be initialized. They are stored in this struct.
@@ -35,7 +36,7 @@ static void display_pre_transfer_callback(spi_transaction_t *t) {
 }
 
 static spi_device_interface_config_t devcfg = { .clock_speed_hz = 1000000, .mode = 0,  //SPI mode 0
-		.spics_io_num = -1,               //CS pin
+		.spics_io_num = DISPLAY_PIN_NUM_CS,               //CS pin
 		.queue_size = 7,                      //We want to be able to queue 7 transactions at a time
 		.pre_cb = display_pre_transfer_callback, //Specify pre-transfer callback to handle D/C line
 		};
@@ -49,12 +50,9 @@ static void send_cmd(spi_device_handle_t spi, const uint8_t cmd) {
 	sendbyte[1] = cmd & 0xF0;
 	sendbyte[2] = (cmd << 4) & 0xF0;
 
-	t.length = sizeof(sendbyte);
+	t.length = sizeof(sendbyte) * 8;
 	t.tx_buffer = sendbyte;
-	gpio_set_level(DISPLAY_PIN_NUM_CS, 1);
 	ESP_ERROR_CHECK(spi_device_transmit(spi, &t));
-	vTaskDelay(10 / portTICK_RATE_MS);
-	gpio_set_level(DISPLAY_PIN_NUM_CS, 0);
 }
 
 static void send_data(spi_device_handle_t spi, const uint8_t data) {
@@ -66,12 +64,9 @@ static void send_data(spi_device_handle_t spi, const uint8_t data) {
 	sendbyte[1] = data & 0xF0;
 	sendbyte[2] = (data << 4) & 0xF0;
 
-	t.length = sizeof(sendbyte);
+	t.length = sizeof(sendbyte) * 8;
 	t.tx_buffer = sendbyte;
-	gpio_set_level(DISPLAY_PIN_NUM_CS, 1);
 	ESP_ERROR_CHECK(spi_device_transmit(spi, &t));
-	vTaskDelay(10 / portTICK_RATE_MS);
-	gpio_set_level(DISPLAY_PIN_NUM_CS, 0);
 }
 
 void display_putstr(spi_device_handle_t spi, const char *str) {
@@ -106,15 +101,11 @@ void display_setpos(spi_device_handle_t spi, int y, int x) {
 }
 
 static void diaplay_spi_init(spi_device_handle_t spi) {
-	ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
-
-	ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
 
 	//Initialize non-SPI GPIOs
 	gpio_set_direction(DISPLAY_PIN_NUM_RST, GPIO_MODE_OUTPUT);
 	gpio_set_direction(DISPLAY_PIN_NUM_BCKL, GPIO_MODE_OUTPUT);
-	gpio_set_direction(DISPLAY_PIN_NUM_CS, GPIO_MODE_OUTPUT);
-	gpio_set_level(DISPLAY_PIN_NUM_CS, 0);
+
 	//Reset the display
 	gpio_set_level(DISPLAY_PIN_NUM_RST, 0);
 	vTaskDelay(100 / portTICK_RATE_MS);
@@ -134,7 +125,14 @@ static void diaplay_spi_init(spi_device_handle_t spi) {
 }
 
 void display_task(void *pvParameters) {
-	static spi_device_handle_t spi;
+	spi_dev_t* spi_hw;
+	ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
+
+	ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
+
+	spi_hw = spicommon_hw_for_host(HSPI_HOST);
+	spi_hw->pin.master_cs_pol = 1;
+
 	diaplay_spi_init(spi);
 
 	while (1) {
